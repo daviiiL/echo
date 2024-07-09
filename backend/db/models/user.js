94f4b85abc02ea85ec304268057104464adbfe68
David Liu <davidas99017@gmail.com>
2024-07-09 16:38:19 -0400

    adding seeders for users

---
+++ backend/db/models/user.js
@@ -0,0 +1,74 @@
+"use strict";
+const { Model, Validator } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  User.init(
+    {
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [4, 30],
+          isNotEmail(value) {
+            if (Validator.isEmail(value)) {
+              throw new Error("Invalid input on field 'email'");
+            }
+          },
+        },
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          len: [3, 256],
+          isEmail: true,
+        },
+      },
+      first_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 30],
+        },
+      },
+      last_name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [1, 30],
+        },
+      },
+      bio: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      stars: { type: DataTypes.INTEGER, allowNull: false },
+      status: { type: DataTypes.STRING, allowNull: false },
+      avatar: { type: DataTypes.STRING, allowNull: true },
+      hashedPassword: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [60, 60],
+        },
+      },
+      last_active: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "User",
+    },
+  );
+  return User;
+};
---
+++ backend/db/seeders/20240709201956-demo-user.js
@@ -0,0 +1,54 @@
+"use strict";
+
+const { User } = require("../models");
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA;
+}
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(_queryInterface, _Sequelize) {
+    await User.bulkCreate([
+      {
+        email: "demo@user.io",
+        username: "Demo-lition",
+        first_name: "Demo",
+        last_name: "Drake",
+        bio: "A test user named Demo Drake",
+        stars: 0,
+        status: "Testing the frontend",
+        hashedPassword: bcrypt.hashSync("password"),
+      },
+      {
+        email: "demo1@user.io",
+        username: "TestingDragon",
+        first_name: "Vlad",
+        last_name: "Dragon",
+        bio: "A test dragon named Vlad Dragon",
+        stars: 0,
+        status: "Testing the backend",
+        hashedPassword: bcrypt.hashSync("password"),
+      },
+      {
+        email: "demo2@user.io",
+        username: "SurfingCheetos",
+        first_name: "Awesome",
+        last_name: "Cheetos",
+        bio: "A test cheetos dwelves in the dark web",
+        stars: 0,
+        status: "Surfing in the bits",
+        hashedPassword: bcrypt.hashSync("password"),
+      },
+    ]);
+  },
+
+  async down(queryInterface, Sequelize) {
+    return queryInterface.bulkDelete("Users", {
+      username: {
+        [Sequelize.Op.in]: ["Demo-lition", "TestingDragon", "SurfingCheetos"],
+      },
+    });
+  },
+};
