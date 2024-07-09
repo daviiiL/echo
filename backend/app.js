9172ac93c609b077ab620c5859f36aa924119937
David Liu <davidas99017@gmail.com>
2024-07-09 15:52:51 -0400

    adding error handling to app.js

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,20 @@
-const express = require('express');
-require('express-async-errors');
-const morgan = require('morgan');
-const cors = require('cors');
-const csurf = require('csurf');
-const helmet = require('helmet');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+require("express-async-errors");
+const morgan = require("morgan");
+const cors = require("cors");
+const csurf = require("csurf");
+const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const { ValidationError } = require("sequelize");
+const { environment } = require("./config");
+const routes = require("./routes");
 
-const { environment } = require('./config');
-const routes = require("./routes"); 
+const isProduction = environment === "production";
 
-const isProduction = environment === 'production';
+const app = express();
+app.use(morgan("dev"));
 
-const app = express(); 
-app.use(morgan('dev')); 
-
-app.use(cookieParser()); 
+app.use(cookieParser());
 app.use(express.json());
 
 // Security Middleware
@@ -26,8 +26,8 @@ if (!isProduction) {
 // helmet helps set a variety of headers to better secure your app
 app.use(
   helmet.crossOriginResourcePolicy({
-    policy: "cross-origin"
-  })
+    policy: "cross-origin",
+  }),
 );
 
 // Set the _csrf token and create req.csrfToken method
@@ -36,11 +36,43 @@ app.use(
     cookie: {
       secure: isProduction,
       sameSite: isProduction && "Lax",
-      httpOnly: true
-    }
-  })
+      httpOnly: true,
+    },
+  }),
 );
+// Catch unhandled requests and forward to error handler.
+app.use((_req, _res, next) => {
+  const err = new Error("The requested resource couldn't be found.");
+  err.title = "Resource Not Found";
+  err.errors = { message: "The requested resource couldn't be found." };
+  err.status = 404;
+  next(err);
+});
+// Process sequelize errors
+app.use((err, _req, _res, next) => {
+  // check if error is a Sequelize error:
+  if (err instanceof ValidationError) {
+    let errors = {};
+    for (let error of err.errors) {
+      errors[error.path] = error.message;
+    }
+    err.title = "Validation error";
+    err.errors = errors;
+  }
+  next(err);
+});
+// Error formatter
+app.use((err, _req, res, _next) => {
+  res.status(err.status || 500);
+  console.error(err);
+  res.json({
+    title: err.title || "Server Error",
+    message: err.message,
+    errors: err.errors,
+    stack: isProduction ? null : err.stack,
+  });
+});
 
-app.use(routes); 
+app.use(routes);
 
-module.exports = app; 
+module.exports = app;
