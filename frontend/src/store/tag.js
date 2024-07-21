fdc15554237b9bc5d24807338c80612911b86669
daviiiL <davidas99017@gmail.com>
2024-07-21 00:21:36 -0400

    added several necessary thunks for tags

--- frontend/src/store/tag.js
+++ frontend/src/store/tag.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { csrfFetch } from "./csrf";
-
+import constructUrlQueryParams from "../utils/constructUrlQueryParams"
 export const fetchActiveTags = createAsyncThunk(
   "tags/fetchActiveTags",
   async (_, { rejectWithValue }) => {
@@ -11,6 +11,24 @@ export const fetchActiveTags = createAsyncThunk(
     else return data;
   }
 );
+
+export const fetchArticleTags = createAsyncThunk("tags/fetchArticleTags", async (articleId, {rejectWithValue}) => {
+  const response = await csrfFetch(`/api/articles/${articleId}/tags`); 
+  const data = await response.json(); 
+  if (response.status >= 400) return rejectWithValue(data); 
+  return data; 
+})
+
+export const postArticletags = createAsyncThunk("tags/postArticletags", 
+async ({articleId, tags}, {rejectWithValue}) => {
+  const response = await csrfFetch(`/api/articles/${articleId}/tags${constructUrlQueryParams(tags, 
+    "tag")}`, {
+      method: "POST"
+  });
+  const data = await response.json(); 
+  if (response.status >= 400) return rejectWithValue(data); 
+  else return data; 
+}) ; 
 
 export const tagSlice = createSlice({
   name: "tags",
@@ -37,6 +55,19 @@ export const tagSlice = createSlice({
     builder.addCase(fetchActiveTags.fulfilled, (state, action) => {
       state.allTags = action.payload.tags;
     }),
+    builder.addCase(postArticletags.rejected, (state, action) => {
+      state.errors = action.payload.errors; 
+    }), 
+    builder.addCase(postArticletags.fulfilled, (state, action) => {
+      state.errors = null; 
+      state.articleTags = action.payload.tags?.map(e => e.title); 
+    }),
+    builder.addCase(fetchArticleTags.rejected, (state, action) => {
+      state.fetchErrors = action.payload.errors; 
+    }), 
+    builder.addCase(fetchArticleTags.fulfilled, (state, action) => {
+      state.articleTags = action.payload.tags?.map(e => e.title); 
+    })
   ],
 });
 
