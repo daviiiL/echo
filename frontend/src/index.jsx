1096b69bc8f55ff1aca82cdc05fb47ea2e3599a1
David <davidas99017@gmail.com>
2024-07-24 19:48:03 -0400

    adding material ui framework support

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,28 +4,29 @@ import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
 import store from "./store";
+import { createTheme } from "@mui/material/styles";
 
-import { restoreCSRF, csrfFetch } from "./store/csrf";
 import { ModalProvider } from "./context/Modal";
-// import { MenuModalProvider } from "./context/MenuModal";
+
 import "@fontsource/roboto";
+import { ThemeProvider } from "styled-components";
 
-if (import.meta.env.MODE !== "production") {
-  restoreCSRF();
-
-  window.csrfFetch = csrfFetch;
-  window.store = store;
-  /*   window.sessionActions = sessionActions; */
-}
+const materialTheme = createTheme({
+  palette: {
+    background: {
+      primary: "#e2e8f0",
+    },
+  },
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ModalProvider>
-      {/* <MenuModalProvider> */}
       <Provider store={store}>
-        <App />
+        <ThemeProvider theme={materialTheme}>
+          <App />
+        </ThemeProvider>
       </Provider>
-      {/* </MenuModalProvider> */}
     </ModalProvider>
   </React.StrictMode>,
 );
