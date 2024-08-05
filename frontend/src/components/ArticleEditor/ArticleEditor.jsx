ccda1e83ef60988c90b09a0904b865e8b7a54166
David <davidas99017@gmail.com>
2024-08-05 13:10:22 -0400

    set user comment cards to none-user-selectable

--- frontend/src/components/ArticleEditor/ArticleEditor.jsx
+++ frontend/src/components/ArticleEditor/ArticleEditor.jsx
@@ -13,11 +13,11 @@ const extensions = [
   StarterKit.configure({
     bulletList: {
       keepMarks: true,
-      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+      keepAttributes: false,
     },
     orderedList: {
       keepMarks: true,
-      keepAttributes: false, // TODO : Making this as `false` becase marks are not preserved when I try to preserve attrs, awaiting a bit of help
+      keepAttributes: false,
     },
   }),
 ];
